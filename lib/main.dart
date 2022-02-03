7530936511e2857bf07e5531c8ef29134af7b46f
IvanYoed <igonzarec@gmail.com>
2022-02-03 00:36:48 -0600

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 // Project imports:
 import 'package:music_management_app/core/presentation/music_management_app.dart';
 
+// Test
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MusicManagementApp());
